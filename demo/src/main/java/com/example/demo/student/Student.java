4f0b40386e0d6ad5600cf6b3cc51f1882e1de1fd
IT22074072 <it22074072@my.sliit.lk>
2024-06-18 16:05:21 +0530

    Student Class

---
+++ demo/src/main/java/com/example/demo/student/Student.java
@@ -0,0 +1,88 @@
+package com.example.demo.student;
+
+import java.time.LocalDate;
+
+public class Student {
+
+    private long id;
+    private String name;
+    private String email;
+    private LocalDate dob;
+    private Integer age;
+
+
+    public Student() {
+    }
+
+    public Student(long id, String name, String email, LocalDate dob, Integer age) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.dob = dob;
+        this.age = age;
+    }
+
+
+    public Student(String name, String email, LocalDate dob, Integer age) {
+        this.name = name;
+        this.email = email;
+        this.dob = dob;
+        this.age = age;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public LocalDate getDob() {
+        return dob;
+    }
+
+    public Integer getAge() {
+        return age;
+    }
+
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setDob(LocalDate dob) {
+        this.dob = dob;
+    }
+
+    public void setAge(Integer age) {
+        this.age = age;
+    }
+
+
+    //ensure that when an object of your class is printed,
+    // it outputs a human-readable and informative string representation
+    @Override
+    public String toString() {
+        return "Student{" +
+                "id=" + id +
+                ", name='" + name + '\'' +
+                ", email='" + email + '\'' +
+                ", dob=" + dob +
+                ", age=" + age +
+                '}';
+    }
+}
+
