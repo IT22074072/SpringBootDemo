8a53c0791b70dd5b1de9d5c71f0f298b72ca196b
IT22074072 <it22074072@my.sliit.lk>
2024-06-19 19:50:44 +0530

    Saving students

---
+++ demo/src/main/java/com/example/demo/student/StudentConfig.java
@@ -0,0 +1,26 @@
+package com.example.demo.student;
+
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.time.LocalDate;
+import java.time.Month;
+import java.util.List;
+
+@Configuration
+public class StudentConfig {
+
+    @Bean
+    CommandLineRunner commandLineRunner(StudentRepository repository){
+        return args -> {
+            Student Dinithi = new Student("Dinithi", "Dinithi@gmail.com", LocalDate.of(2002, Month.OCTOBER, 13), 21);
+            Student Piyumi = new Student("Piyumi", "Piyumi@gmail.com", LocalDate.of(1998, Month.JUNE, 13), 26);
+
+            repository.saveAll(
+                    List.of(Dinithi, Piyumi)
+            );
+
+        };
+    }
+}
