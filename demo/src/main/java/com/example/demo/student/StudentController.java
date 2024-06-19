ea1fbff814288738577df49f26a73726697f6c63
IT22074072 <it22074072@my.sliit.lk>
2024-06-19 08:52:27 +0530

    Creating and connecting to Database

--- demo/src/main/java/com/example/demo/student/StudentController.java
+++ demo/src/main/java/com/example/demo/student/StudentController.java
@@ -9,20 +9,34 @@ import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
 
+// REST controllers handle HTTP requests and return JSON responses.
 @RestController
+
+// Annotation to map HTTP requests to this controller. The path specified will be the base URL for all requests in this controller.
 @RequestMapping(path="api/v1/student")
+
 
 public class StudentController {
 
+    // Declaring a final variable for the StudentService, which will be injected by Spring.
     private final StudentService studentService;
 
+
+    // Constructor for the StudentController, with the StudentService being injected by Spring.
+    // The @Autowired annotation indicates that the StudentService should be injected automatically.
     @Autowired
     public StudentController(StudentService studentService) {
         this.studentService = studentService;
     }
 
+
+
+    // Mapping for HTTP GET requests to the base URL.
+    // This method will be called when a GET request is made to "api/v1/student".
     @GetMapping
     public List<Student> getStudents(){
+        // This method calls the getStudents() method of the StudentService
+        // and returns the list of students.
         return studentService.getStudents();
 
     }
--- demo/src/main/java/com/example/demo/student/StudentService.java
+++ demo/src/main/java/com/example/demo/student/StudentService.java
@@ -7,9 +7,13 @@ import java.time.LocalDate;
 import java.time.Month;
 import java.util.List;
 
+
+
 @Service
 public class StudentService {
+    // Method to get a list of students.
     public List<Student> getStudents(){
+        // Returning a list with one Student object.
         return List.of(new Student(1L, "Dinithi", "Dinithi@gmail.com", LocalDate.of(2002, Month.OCTOBER, 13), 21)) ;
     }
 }
